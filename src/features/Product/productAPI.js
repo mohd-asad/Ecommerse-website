46c2cba789e016fb98eacbaad04511bd37d33c9f
Mohd Asad~ <mohdasad024@gmail.com>
2024-01-03 19:48:40 +0530

    filters

--- src/features/Product/productAPI.js
+++ src/features/Product/productAPI.js
@@ -1,25 +1,32 @@
 export function fetchAllProducts() {
-  return new Promise(async (resolve) =>{
+  return new Promise(async (resolve) => {
     //todo
-    const response =await fetch('http://localhost:8080/products')
-    const data =await response.json()
-    resolve({data})
-  }
-  );
+    const response = await fetch("http://localhost:8080/products");
+    const data = await response.json();
+    resolve({ data });
+  });
 }
 
-export function fetchProductsByFilters(filter) {
-
-  let queryString='';
-  for(let key in filter){
-    queryString +=`${key}=${filter[key]}&`
+export function fetchProductsByFilters(filter,sort) {
+  let queryString = "";
+  for (let key in filter) {
+    const categoryValues = filter[key];
+    if (categoryValues.length) {
+      const lastCategoryValue = categoryValues[categoryValues.length - 1];
+      queryString += `${key}=${lastCategoryValue}&`;
+    }
   }
 
-  return new Promise(async (resolve) =>{
+  for(let key in sort){
+    queryString+=`${key}=${sort[key]}&`;
+  }
+
+  return new Promise(async (resolve) => {
     //todo
-    const response =await fetch('http://localhost:8080/products?'+queryString)
-    const data =await response.json()
-    resolve({data})
-  }
-  );
+    const response = await fetch(
+      "http://localhost:8080/products?" + queryString
+    );
+    const data = await response.json();
+    resolve({ data });
+  });
 }
--- src/features/Product/productSlice.js
+++ src/features/Product/productSlice.js
@@ -1,13 +1,13 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { fetchAllProducts,fetchProductsByFilters } from './productAPI';
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { fetchAllProducts, fetchProductsByFilters } from "./productAPI";
 
 const initialState = {
   products: [],
-  status: 'idle',
+  status: "idle",
 };
 
 export const fetchAllProductsAsync = createAsyncThunk(
-  'product/fetchAllProducts',
+  "product/fetchAllProducts",
   async () => {
     const response = await fetchAllProducts();
     // The value we return becomes the `fulfilled` action payload
@@ -16,46 +16,44 @@ export const fetchAllProductsAsync = createAsyncThunk(
 );
 
 export const fetchProductsByFiltersAsync = createAsyncThunk(
-  'product/fetchProductsByFilters',
-  async (filter) => {
-    const response = await fetchProductsByFilters(filter);
+  "product/fetchProductsByFilters",
+  async ({ filter, sort }) => {
+    const response = await fetchProductsByFilters(filter, sort);
     // The value we return becomes the `fulfilled` action payload
     return response.data;
   }
 );
 
 export const productSlice = createSlice({
-  name: 'product',
+  name: "product",
   initialState,
 
   reducers: {
     increment: (state) => {
-    
       state.value += 1;
     },
   },
- 
+
   extraReducers: (builder) => {
     builder
       .addCase(fetchAllProductsAsync.pending, (state) => {
-        state.status = 'loading';
+        state.status = "loading";
       })
       .addCase(fetchAllProductsAsync.fulfilled, (state, action) => {
-        state.status = 'idle';
+        state.status = "idle";
         state.products = action.payload;
       })
       .addCase(fetchProductsByFiltersAsync.pending, (state) => {
-        state.status = 'loading';
+        state.status = "loading";
       })
       .addCase(fetchProductsByFiltersAsync.fulfilled, (state, action) => {
-        state.status = 'idle';
+        state.status = "idle";
         state.products = action.payload;
       });
   },
 });
 
 export const { increment } = productSlice.actions;
-
 
 export const selectAllProducts = (state) => state.product.products;
 
